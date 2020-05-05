da32d6b811683667658818b21d371a8f50397af0
Atsu1314 <moririn32@gmail.com>
2020-05-05 14:27:59 +0900

    add animation

--- portfolio_project/src/components/parts/Top.vue
+++ portfolio_project/src/components/parts/Top.vue
@@ -1,15 +1,24 @@
 <template id="">
-  <div class="jumbotron junbotron-extend">
+  <div class="jumbotron junbotron-extend" :class="{welcome : get_in}">
     <div class="container-fluid text-center">
       Wlcome to <p class="name">Muta Atsuyoshi's</p> Portfolio!
     </div>
-    <div class="btn"><p>Get in</p></div>
+    <div class="btn" @click='welcome' :class="{clicked : get_in}"><p>Get in</p></div>
   </div>
 </template>
 
 <script type="text/javascript">
   export default {
-
+    data() {
+      return {
+      get_in : false,
+      }
+    },
+    methods: {
+      welcome() {
+        this.get_in = !this.get_in
+      }
+    }
   }
 </script>
 
@@ -19,6 +28,7 @@
   position:relative;
   height: 100vh;
   background: url("../../assets/IMG_0167.jpg") no-repeat center center;
+  background-color: #aaa;
   background-size: cover;
 }
 
@@ -27,13 +37,13 @@ div{
   font-family: 'Sawarabi Mincho', sans-serif;
   font-size: 90px;
   user-select: none;
-  text-shadow: 4px 4px 4px #aaa;
+  /* text-shadow: 4px 4px 4px #aaa; */
   spacing: 0.1em;
   line-height: 1.2em;
 }
 
 .name{
-  color:#FFF5EE;
+  color:white;
 }
 
 .container-fluid{
@@ -47,6 +57,7 @@ div{
   border-radius: 10px;
   border-color: white;
   text-shadow: none;
+  margin-top: 100px;
 }
 
 .btn:hover{
@@ -56,4 +67,13 @@ div{
   text-decoration: none;
 }
 
+.clicked{
+  color: white!important;
+  transition: all 600ms 0s ease;
+}
+
+.welcome{
+  background: white;
+  transition: all 600ms 0s ease;
+}
 </style>
