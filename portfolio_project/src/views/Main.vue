eb771fa2d6a34584ceaa9b28a49950ff4ec90b61
Atsu1314 <moririn32@gmail.com>
2020-08-01 16:14:20 +0900

    modified

--- portfolio_project/src/views/Main.vue
+++ portfolio_project/src/views/Main.vue
@@ -3,13 +3,38 @@
     <div v-show="move1" class="btn row w-75" :class="{btn_after : move2}">
       <div class="row">
         <b class="h1 history col-12">H I S T O R Y</b>
-        <p class="col">2014/04 千葉大学 理学部生物学科 入学</p>
+        <div class="w-100 row">
+          <div class="col-4 p-0">2014/04</div>
+          <div class="col-8 p-0">千葉大学 理学部生物学科 入学</div>
+        </div>
+        <div class="w-100 row">
+          <div class="col-4 p-0">2015/04</div>
+          <div class="col-8 p-0">千葉大学 数学・情報数理学科 転学科</div>
+        </div>
+        <div class="w-100 row">
+          <div class="col-4 p-0">2018/04</div>
+          <div class="col-8 p-0">東京工業大学 情報理工学院 数理・計算科学系 数理計算科学コース 入学</div>
+        </div>
+        <div class="w-100 row">
+          <div class="col-4 p-0">2020/02</div>
+          <div class="col-8 p-0">イグニションポイント株式会社 インターンとしてフロントエンド開発を経験</div>
+        </div>
+        <div class="w-100 row">
+          <div class="col-4 p-0">2020/09</div>
+          <div class="col-8 p-0">東京工業大学 情報理工学院 数理・計算科学系 数理計算科学コース 修了予定</div>
+        </div>
+        <!-- <p class="col">2014/04 千葉大学 理学部生物学科 入学</p>
         <p class="col-12">2015/04 千葉大学 数学・情報数理学科 転学科</p>
         <p class="col-12">2018/04 東京工業大学 情報理工学院 数理・計算科学系 数理計算科学コース 入学</p>
-        <p class="col-12">2020/02 イグニションポイント株式会社 インターンとしてフロントエンド開発を経験</p>
+        <p class="col-12">2020/02 イグニションポイント株式会社 インターンとしてフロントエンド開発を経験</p> -->
       </div>
+
       <div class="row">
-        <b class="h1 col-12 works">W O R K s</b>
+        <b class="h1 col-12 works">W O R K S</b>
+        <div class="w-100 row">
+          <div class="col-4 p-0">2020/08</div>
+          <div class="col-8 p-0">習作としてJavaScriptのフレームワークVue.jsを用いて<a href="" class="href">本ポートフォリオサイト</a>を自作．ソースコードはGitHub上で公開しています．</div>
+        </div>
       </div>
     </div>
   </div>
@@ -83,5 +108,9 @@ div{
 
 .works{
   padding-top: 30px;
+}
+
+.href{
+  color:red;
 }
 </style>
